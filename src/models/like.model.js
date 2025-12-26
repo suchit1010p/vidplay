02cb445d54fae476115b24789e93e1fbd8b1fc84
suchit1010p <suchit1564@gmail.com>
2025-12-26 22:06:27 +0530

    add models for comments, likes, and playlists; establish schema definitions and relationships

---
+++ src/models/like.model.js
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from "mongoose";
+
+const likeSchema = new Schema({
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+        required: true
+    },
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+},{ timestamps: true });
+
+export const Like = mongoose.model("Like", likeSchema);
---
+++ src/models/playlist.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        maxlength: 500,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    videos: [{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }]
+}, { timestamps: true });
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
