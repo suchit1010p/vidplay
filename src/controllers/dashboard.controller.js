17fd5a056b779de582bba8a61f17984a1e25d7d5
suchit1010p <suchit1564@gmail.com>
2026-01-01 10:46:42 +0530

    refactor: remove unused imports in dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,10 +1,5 @@
-import {isValidObjectId} from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { Like } from "../models/like.model.js";
-import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { Comment } from "../models/comment.model.js";
-import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
